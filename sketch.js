3ab425fe917f6f7b9f0531ca714c84c111be26c4
anchal <anchalsoni887@yahoo.com>
2021-01-18 16:20:10 +0530

    added the platform

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    platform=new Ground(150,305,300,170)
+   platform=new Ground(400,400,200,200)
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,720,70,100);
@@ -45,6 +45,7 @@ function draw(){
     // console.log(box2.body.angle);
 
     box1.display();
+
     box2.display();
     ground.display();
     viking1.display();
